af40a594a0358b77871d29ae807a915cefb19c9f
Jacqui Kientzler ✨ <jacqui.kientzler@me.com>
2023-09-16 14:05:20 -0700

    edit: adjust chatgpt prompt generation parameters

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -14,6 +14,7 @@ const configuration = new Configuration({
 type PromptResponse = {
   animal: string;
   keywords: string;
+  setting: string;
   style: string;
 }
 
@@ -22,44 +23,40 @@ const styles: Array<string> = ["watercolor", "anime", "oil paint", "sketch", "ph
 const settings: Array<string> = ["forest", "desert", "cafe", "living room", "meadow", "blanket pile"];
 const gptPromptTrainingData: ChatCompletionRequestMessage[] = [
   {
-    role: "system",
-    content: "You are a creative prompt engineer who thinks up prompts for AI text to image generation. Your prompts all involve animals, and aim to produce cute, cozy images that will give viewers a feeling of warmth and contentment. Your prompts use keywords and short phrases, rather than full sentences, and always include at least two words describing the style of the generated image. The prompts are provided as a JSON object that follows the following schema: \n\n`{ \"animal\": string, \"keywords\": string, \"style\": string }`"
+    "role": "system",
+    "content": "You are a creative prompt engineer who thinks up prompts for AI text to image generation. Your prompts all involve domesticated mammals, and aim to produce cute, cozy images that will give viewers a feeling of warmth and contentment. Your prompts use keywords and short phrases which do not use compositionality, always include keywords describing the setting/background of the image, and always include at least two words describing the style of the generated image. The prompts are provided as a JSON object that follows the following schema: \n\n`{ \"animal\": string, \"keywords\": string, \"setting\": string, \"style\": string }`\n\nExamples of good styles to use: impressionist, soft focus, oil painting, watercolor, 3D rendering, digital illustration, anime, kawaii\n\nExamples of bad styles that you should not use: photorealistic, photography, minimalistic, cartoon, pastel, hd, high quality"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"puppy\", \"keywords\": \"snuggled up\", \"style\": \"watercolor\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"dog\", \"keywords\": \"knitting, fireplace\", \"setting\": \"cozy living room\", \"style\": \"watercolor\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"kitten\", \"keywords\": \"curled up\", \"style\": \"pastel\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"cat\", \"keywords\": \"reading, window view\", \"setting\": \"sunlit study\", \"style\": \"soft focus\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"bunny\", \"keywords\": \"cozy nest\", \"style\": \"chalk drawing\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"rabbit\", \"keywords\": \"hot chocolate, snow\", \"setting\": \"winter cabin\", \"style\": \"impressionist\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"panda\", \"keywords\": \"hugging tree\", \"style\": \"cartoon\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"hamster\", \"keywords\": \"baking, kitchen counter\", \"setting\": \"rustic farmhouse kitchen\", \"style\": \"oil painting\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"duckling\", \"keywords\": \"under a rainbow\", \"style\": \"digital painting\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"guinea pig\", \"keywords\": \"flower crown, picnic blanket\", \"setting\": \"blooming garden\", \"style\": \"3D rendering\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"teddy bear\", \"keywords\": \"sitting on a shelf\", \"style\": \"knitted\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"dog\", \"keywords\": \"painting, art supplies\", \"setting\": \"art studio\", \"style\": \"digital illustration\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"baby seal\", \"keywords\": \"playing in snow\", \"style\": \"photorealistic\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"cat\", \"keywords\": \"tea, books\", \"setting\": \"cozy library\", \"style\": \"anime\"}"
   },
   {
-    role: "assistant",
-    content: "{\"animal\": \"elephant\", \"keywords\": \"splashing in water\", \"style\": \"oil painting\"}"
-  },
-  {
-    role: "assistant",
-    content: "{\"animal\": \"penguin\", \"keywords\": \"waddling on ice\", \"style\": \"sketch\"}"
+    "role": "assistant",
+    "content": "{\"animal\": \"rabbit\", \"keywords\": \"gardening, sun hat\", \"setting\": \"enchanted garden\", \"style\": \"kawaii\"}"
   }
 ];
 
@@ -82,15 +79,15 @@ async function getGenerativePromptText(): Promise<string> {
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: gptPromptTrainingData,
-    temperature: 0.99,
+    temperature: 1.2,
     max_tokens: 256,
     top_p: 1,
-    frequency_penalty: 0.77,
+    frequency_penalty: 0,
     presence_penalty: 0,
   }).then(res => res.data.choices[0].message?.content);
   if(response){
     const responseObj = JSON.parse(response) as PromptResponse;
-    const prompt = responseObj.animal + " " + responseObj.keywords + ", " + responseObj.style + " style";
+    const prompt = responseObj.animal + " " + responseObj.keywords + ", " + responseObj.setting + ", " + responseObj.style + " style";
     return prompt;
   } else {
     return "error symbol";
