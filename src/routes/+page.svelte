21f556e3c4b520e12b2c3be4ffd709766bc43d32
Jacqui Kientzler ✨ <jacqui.kientzler@me.com>
2023-09-16 14:05:20 -0700

    code: add types to prompt generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  let animalNames = ["cat", "dog", "horse", "kitten", "puppy", "penguin", "bunny"];
-  let styles = ["watercolor", "anime", "oil paint", "sketch", "photorealistic", "renaissance", "impressionist", "3D rendering"];
-  let settings = ["forest", "desert", "cafe", "living room", "meadow", "blanket pile"];
+  let animalNames: Array<string> = ["cat", "dog", "horse", "kitten", "puppy", "penguin", "bunny"];
+  let styles: Array<string> = ["watercolor", "anime", "oil paint", "sketch", "photorealistic", "renaissance", "impressionist", "3D rendering"];
+  let settings: Array<string> = ["forest", "desert", "cafe", "living room", "meadow", "blanket pile"];
 
-  function getRandomIntInclusive(max: number) {
-  let min = Math.ceil(0);
+  function getRandomIntInclusive(max: number): number {
+  let min: number = Math.ceil(0);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
 
-  function getRandomImagePrompt() {
-    let animal = animalNames[getRandomIntInclusive(animalNames.length - 1)];
-    let style = styles[getRandomIntInclusive(styles.length - 1)];
-    let setting = settings[getRandomIntInclusive(settings.length - 1)];
+  function getRandomImagePrompt(): string {
+    let animal: string = animalNames[getRandomIntInclusive(animalNames.length - 1)];
+    let style: string = styles[getRandomIntInclusive(styles.length - 1)];
+    let setting: string = settings[getRandomIntInclusive(settings.length - 1)];
     return animal + " in " + setting + " " + style
   }
 
