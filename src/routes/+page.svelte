d4425580eff17ed1d77c51d37f0d91bb06f6fb32
Jacqui Kientzler ✨ <jacqui.kientzler@me.com>
2023-09-16 14:09:43 -0700

    fix: address loading perf with proper await of image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   export let data;
-  let promise = data.imageURL;
 </script>
 
 <h1>Welcome to Jacqui's Poster Shop</h1>
 <p>I want to make cozy animal posters here.</p>
 <p>Your prompt is: {data.imagePrompt}</p>
-{#await promise}
+{#await data.streamed.imagePromise}
   <p>awaiting generated image</p>
-{:then image_url}
-  <img src={image_url} alt="generated AI art"/>
+{:then imageURL}
+  <img src={imageURL} alt="generated AI art"/>
 {:catch error}
   <p>There was an issue getting the image.</p>
 {/await}
