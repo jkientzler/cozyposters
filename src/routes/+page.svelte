505e4b41ee0169bbeeaf735b7a31c23f5e041e51
Jacqui Kientzler ✨ <jacqui.kientzler@me.com>
2025-01-29 15:06:10 -0800

    code: modify homepage text, add turnstile pkg, npm audit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,13 @@
   export let data;
 </script>
 
-<h1>Welcome to Jacqui's Poster Shop</h1>
-<p>I want to make cozy animal posters here.</p>
-<p>Your prompt is: {data.imagePrompt}</p>
+<h1>cozyposters</h1>
+<p>An endless stream of adorable posters, just refresh the page for a new one.</p>
+<p>ChatGPT generated the following prompt: {data.imagePrompt}</p>
 {#await data.streamed.replicateOutputPromise}
-  <p>awaiting generated Replicate image</p>
+  <p>awaiting generated image from Stable Diffusion</p>
 {:then imageURL}
-  <h1>Replicate</h1>
   <img src={imageURL} alt="generated AI art from Replicate"/>
 {:catch error}
-  <p>There was an issue getting the image.</p>
+  <p>There was an issue getting the image. Please refresh the page to try again.</p>
 {/await}
